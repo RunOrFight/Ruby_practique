610ebe1de9344f1ec6c5ad563750552f2bfeb3f8
RunOrFight <dragoinfury@gmail.com>
2021-06-23 11:27:25 +0300

    Add pages and ref

--- Barbershop/app.rb
+++ Barbershop/app.rb
@@ -3,3 +3,15 @@ require 'sinatra'
 get '/' do
   erb "Hello all Ruby-programmers!"
 end
+
+get '/about' do 
+  erb :about
+end
+
+get '/visit' do
+  erb :visit
+end
+
+get '/contacts' do
+  erb :contacts
+end
